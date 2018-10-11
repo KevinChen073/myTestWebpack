42ab1bd3bd35e2e86ed21893bb4ed3e7e5c88714
KevinChen073 <604103356@qq.com>
2018-10-11 21:18:48 +0800

    小修改颜色

--- src/commands/ownWork.js
+++ src/commands/ownWork.js
@@ -6,6 +6,11 @@ module.exports = ()=>{
     let index = readLineSync.keyInSelect(workConfig.dataSource.map((v)=>{
         return v.desc;
     }), '选择需要的日常');
-    let work = workConfig.dataSource[index];
-    console.log(`选择了：${work.desc} : ${work.alias}`.green);
+    if (index !== -1) {
+        let work = workConfig.dataSource[index];
+        console.log(`选择了：${work.desc} : ${work.alias}`.green);
+    } else {
+        console.log(`放弃选择`.cyan);
+    }
+    process.exit(0);
 }
